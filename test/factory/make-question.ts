b832aa98e7d338c1e33fd3883328cade4b713ef9
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2026-03-06 22:30:35 +0000

    test: migrate test utilities from forum core
    Moves existing test factories, in-memory repositories, and utility functions from the core forum application into the NestJS project structure.

---
+++ test/factory/make-question.ts
@@ -0,0 +1,23 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import {
+  Question,
+  QuestionProps,
+} from "@/domain/forum/enterprise/entities/question";
+import { faker } from "@faker-js/faker";
+
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      content: faker.lorem.text(),
+      title: faker.lorem.sentence(),
+      ...override,
+    },
+    id
+  );
+
+  return question;
+}
