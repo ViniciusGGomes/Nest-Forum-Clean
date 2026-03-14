37371a6276d0f9d0dddd0d7ed5af496802d7164f
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2026-03-14 23:30:39 +0000

    test(factory): add prisma question comment factory for e2e tests
    Introduces QuestionCommentFactory with the makePrismaQuestionComment method to persist question comment entities in the database using Prisma for e2e testing.

--- test/factory/make-question-comment.ts
+++ test/factory/make-question-comment.ts
@@ -4,10 +4,13 @@ import {
   QuestionCommentProps,
 } from "@/domain/forum/enterprise/entities/question-comment";
 import { faker } from "@faker-js/faker";
+import { PrismaQuestionCommentMapper } from "@infra/database/prisma/mappers/prisma-question-comment-mapper";
+import { PrismaService } from "@infra/database/prisma/prisma.service";
+import { Injectable } from "@nestjs/common";
 
 export function makeQuestionComment(
   override: Partial<QuestionCommentProps> = {},
-  id?: UniqueEntityID
+  id?: UniqueEntityID,
 ) {
   const questionComment = QuestionComment.create(
     {
@@ -16,8 +19,25 @@ export function makeQuestionComment(
       content: faker.lorem.text(),
       ...override,
     },
-    id
+    id,
   );
 
   return questionComment;
+}
+
+@Injectable()
+export class QuestionCommentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestionComment(
+    data: Partial<QuestionCommentProps> = {},
+  ): Promise<QuestionComment> {
+    const questionComment = makeQuestionComment(data);
+
+    await this.prisma.comment.create({
+      data: PrismaQuestionCommentMapper.toPrisma(questionComment),
+    });
+
+    return questionComment;
+  }
 }
