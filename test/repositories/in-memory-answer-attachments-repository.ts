4ae71546f55ecf80d95437a1aab59aef832fd8c2
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2026-04-05 20:30:22 +0000

    test(repositories): implement bulk operations for answer attachments
    Updates the InMemoryAnswerAttachmentsRepository with  and  methods. Integrates these operations into the InMemoryAnswersRepository to correctly sync the answer's watched list (new and removed attachments) during save and create actions.

--- test/repositories/in-memory-answer-attachments-repository.ts
+++ test/repositories/in-memory-answer-attachments-repository.ts
@@ -6,9 +6,21 @@ export class InMemoryAnswerAttachmentsRepository
 {
   public items: AnswerAttachment[] = [];
 
-  async findManyByAnswerId(questionId: string) {
+    async createMany(attachments: AnswerAttachment[]): Promise<void> {
+      this.items.push(...attachments);
+    }
+    
+    async deleteMany(attachments: AnswerAttachment[]): Promise<void> {
+      const answerAttachments = this.items.filter((item) => {
+        return !attachments.some((attachment) => attachment.equals(item));
+      });
+  
+      this.items = answerAttachments;
+    }
+
+  async findManyByAnswerId(answerId: string) {
     const answerAttachments = this.items.filter(
-      (item) => item.answerId.toString() === questionId
+      (item) => item.answerId.toString() === answerId
     );
 
     return answerAttachments;
