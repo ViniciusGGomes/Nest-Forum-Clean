23adb47c4211260a7020c5e01ad1c86ff6cc92b0
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2026-04-06 01:18:13 +0000

    feat(infra/database): implement bulk operations for question attachments
    Implements the createMany and deleteMany methods in the PrismaQuestionAttachmentsRepository to efficiently handle batch updates and deletions of attachments linked to questions.

--- src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
@@ -2,11 +2,37 @@ import { QuestionAttachmentsRepository } from "@domain/forum/application/reposit
 import { QuestionAttachment } from "@domain/forum/enterprise/entities/question-attachment";
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma.service";
-import { PrismaQuestionAttachmentMapper } from "../mappers/prisma-question-attachment";
+import { PrismaQuestionAttachmentMapper } from "../mappers/prisma-question-attachment-mapper";
 
 @Injectable()
 export class PrismaQuestionAttachmentsRepository implements QuestionAttachmentsRepository {
   constructor(private prisma: PrismaService) {}
+  async createMany(attachments: QuestionAttachment[]): Promise<void> {
+    if (attachments.length === 0) {
+      return;
+    }
+
+    const data = PrismaQuestionAttachmentMapper.toPrismaUpdateMany(attachments);
+
+    await this.prisma.attachment.updateMany(data);
+  }
+  async deleteMany(attachments: QuestionAttachment[]): Promise<void> {
+    if (attachments.length === 0) {
+      return;
+    }
+
+    const attachmentIds = attachments.map((attachment) => {
+      return attachment.attachmentId.toString();
+    });
+
+    await this.prisma.attachment.deleteMany({
+      where: {
+        id: {
+          in: attachmentIds,
+        },
+      },
+    });
+  }
 
   async findManyByQuestionId(
     questionId: string,
