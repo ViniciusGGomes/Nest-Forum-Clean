6cc6f6b24b1025bec60d15d522215f97a1785c6f
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2026-04-07 19:27:36 +0000

    test(e2e): verify attachment persistence on answer creation
    Updates the answer question E2E test to include attachments in the request payload. Utilizes the AttachmentFactory to generate mock attachments and asserts that they are correctly linked to the newly created answer in the database.

--- src/infra/http/controllers/answer-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/answer-question.controller.e2e-spec.ts
@@ -7,23 +7,26 @@ import { StudentFactory } from "test/factory/make-student";
 import request from "supertest";
 import { DatabaseModule } from "@infra/database/database.module";
 import { PrismaService } from "@infra/database/prisma/prisma.service";
+import { AttachmentFactory } from "test/factory/make-attachment";
 
 describe("Answer Question (E2E)", () => {
   let app: INestApplication;
   let studentFactory: StudentFactory;
   let questionFactory: QuestionFactory;
+  let attachmentFactory: AttachmentFactory;
   let jwt: JwtService;
   let prisma: PrismaService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, QuestionFactory],
+      providers: [StudentFactory, QuestionFactory, AttachmentFactory],
     }).compile();
 
     app = moduleRef.createNestApplication();
     studentFactory = moduleRef.get(StudentFactory);
     questionFactory = moduleRef.get(QuestionFactory);
+    attachmentFactory = moduleRef.get(AttachmentFactory);
     jwt = moduleRef.get(JwtService);
     prisma = moduleRef.get(PrismaService);
 
@@ -41,11 +44,15 @@ describe("Answer Question (E2E)", () => {
 
     const questionId = question.id.toString();
 
+    const attachment1 = await attachmentFactory.makePrismaAttachment();
+    const attachment2 = await attachmentFactory.makePrismaAttachment();
+
     const response = await request(app.getHttpServer())
       .post(`/questions/${questionId}/answer`)
       .set("Authorization", `Bearer ${accessToken}`)
       .send({
         content: "new answer",
+        attachments: [attachment1.id.toString(), attachment2.id.toString()],
       });
 
     expect(response.statusCode).toBe(201);
@@ -59,5 +66,13 @@ describe("Answer Question (E2E)", () => {
     });
 
     expect(answerOnDatabase).toBeTruthy();
+
+    const attachmentOnDatabase = await prisma.attachment.findMany({
+      where: {
+        answerId: answerOnDatabase?.id,
+      },
+    });
+
+    expect(attachmentOnDatabase).toHaveLength(2);
   });
 });
