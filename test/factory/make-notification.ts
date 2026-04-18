d066757e37edd7bf398c3449d9a5358dacbd7e6a
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2026-04-18 16:18:23 +0000

    test(factory): add notification factory for e2e tests
    Creates the NotificationFactory and makeNotification helper function to easily generate and persist mock Notification entities in the database, streamlining the setup process for automated tests.

--- test/factory/make-notification.ts
+++ test/factory/make-notification.ts
@@ -4,10 +4,13 @@ import {
   NotificationProps,
 } from "@/domain/notification/enterprise/entities/notification";
 import { faker } from "@faker-js/faker";
+import { PrismaNotificationMapper } from "@infra/database/prisma/mappers/prisma-notification-mapper";
+import { PrismaService } from "@infra/database/prisma/prisma.service";
+import { Injectable } from "@nestjs/common";
 
 export function makeNotification(
   override: Partial<NotificationProps> = {},
-  id?: UniqueEntityID
+  id?: UniqueEntityID,
 ) {
   const notification = Notification.create(
     {
@@ -16,8 +19,25 @@ export function makeNotification(
       content: faker.lorem.text(),
       ...override,
     },
-    id
+    id,
   );
 
   return notification;
+}
+
+@Injectable()
+export class NotificationFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaNotification(
+    data: Partial<NotificationProps> = {},
+  ): Promise<Notification> {
+    const notification = makeNotification(data);
+
+    await this.prisma.notification.create({
+      data: PrismaNotificationMapper.toPrisma(notification),
+    });
+
+    return notification;
+  }
 }
